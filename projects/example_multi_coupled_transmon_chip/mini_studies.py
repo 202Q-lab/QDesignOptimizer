3d9fd5979c88f80e8aa07e28d875cc23bcb2a81b
Pietro Campana <campana@chalmers.se>
2025-02-04 10:50:29 +0100

    Clean examples

--- projects/example_multi_coupled_transmon_chip/mini_studies.py
+++ projects/example_multi_coupled_transmon_chip/mini_studies.py
@@ -1,32 +1,33 @@
+import design_variables as dv
 import numpy as np
-from src.qdesignoptimizer.design_analysis_types import MiniStudy
-from src.utils.utils_design_variables import junction_setup
-import design_variables as dv 
-import src.utils.constants as dc
 import target_parameters as tp
 
-CONVERGENCE = dict(nbr_passes = 7, delta_f = 0.03)
+import qdesignoptimizer.utils.constants as dc
+from qdesignoptimizer.design_analysis_types import MiniStudy
+from qdesignoptimizer.utils.utils_design_variables import junction_setup
+
+CONVERGENCE = dict(nbr_passes=7, delta_f=0.03)
+
 
 def get_mini_study_qb_res(branch: int):
     return MiniStudy(
-        component_names = [
-            dv.name_qb(branch),
-            dv.name_res(branch),
-            dv.name_tee(branch)
+        component_names=[dv.name_qb(branch), dv.name_res(branch), dv.name_tee(branch)],
+        port_list=[
+            (dv.name_tee(branch), "prime_end", 50),
+            (dv.name_tee(branch), "prime_start", 50),
         ],
-        port_list =  [(dv.name_tee(branch),'prime_end', 50),
-                    (dv.name_tee(branch),'prime_start', 50),],
-        open_pins = [],
-        mode_freqs = [
+        open_pins=[],
+        mode_freqs=[
             (str(branch), dc.QUBIT_FREQ),
             (str(branch), dc.RES_FREQ),
         ],
-        jj_var = dv.JUNCTION_VARS,
-        jj_setup = {**junction_setup(dv.name_qb(branch))},
-        design_name = "get_mini_study_qb_res",
-        adjustment_rate = 0.8,
+        jj_var=dv.JUNCTION_VARS,
+        jj_setup={**junction_setup(dv.name_qb(branch))},
+        design_name="get_mini_study_qb_res",
+        adjustment_rate=0.8,
         **CONVERGENCE
     )
+
 
 def get_mini_study_2qb_resonator_coupler(branches: list, coupler: int):
     all_comps = []
@@ -35,7 +36,12 @@ def get_mini_study_2qb_resonator_coupler(branches: list, coupler: int):
     all_jjs = {}
     for branch in branches:
         all_comps.extend([dv.name_qb(branch), dv.name_res(branch), dv.name_tee(branch)])
-        all_ports.extend([(dv.name_tee(branch),'prime_end', 50), (dv.name_tee(branch),'prime_start', 50),])
+        all_ports.extend(
+            [
+                (dv.name_tee(branch), "prime_end", 50),
+                (dv.name_tee(branch), "prime_start", 50),
+            ]
+        )
         all_modes.extend([(str(branch), dc.QUBIT_FREQ), (str(branch), dc.RES_FREQ)])
         all_jjs.update(junction_setup(dv.name_qb(branch)))
 
@@ -46,15 +52,15 @@ def get_mini_study_2qb_resonator_coupler(branches: list, coupler: int):
     for i in range(len(all_modes)):
         all_mode_freq.append(tp.TARGET_PARAMS[all_modes[i][0]][all_modes[i][1]])
     all_modes_sorted = [all_modes[i] for i in np.argsort(all_mode_freq)]
-    
+
     return MiniStudy(
-        component_names = all_comps ,
-        port_list =  all_ports,
-        open_pins = [],
-        mode_freqs = all_modes_sorted,
-        jj_var = dv.JUNCTION_VARS,
-        jj_setup = all_jjs,
-        design_name = "get_mini_study_2qb_resonator_coupler",
-        adjustment_rate = 0.8,
+        component_names=all_comps,
+        port_list=all_ports,
+        open_pins=[],
+        mode_freqs=all_modes_sorted,
+        jj_var=dv.JUNCTION_VARS,
+        jj_setup=all_jjs,
+        design_name="get_mini_study_2qb_resonator_coupler",
+        adjustment_rate=0.8,
         **CONVERGENCE
     )
