ebaa466dee16dcec8d5958e0b6e1e7f11a4f66e8
Axel <marer@chalmers.se>
2025-02-27 12:03:34 +0100

    discard_design_var_single

--- src/qdesignoptimizer/utils/optimization_targets.py
+++ src/qdesignoptimizer/utils/optimization_targets.py
@@ -7,6 +7,13 @@ import qdesignoptimizer.utils.names_design_variables as n
 from qdesignoptimizer.design_analysis_types import OptTarget
 from qdesignoptimizer.utils.names_parameters import Mode, param, param_nonlin
 
+discard_design_var_single = lambda x: 1
+"""Used to discard the design variable dependency in the prop_to function of the OptTarget
+for a design_var defined by a single input ."""
+discard_design_var_double = lambda x, y: 1
+"""Used to discard the design variable dependency in the prop_to function of the OptTarget
+for a design_var defined by two inputs ."""
+
 
 def get_opt_target_qubit_freq_via_lj(
     qubit: Mode,
@@ -78,6 +85,7 @@ def get_opt_target_res_qub_chi_via_coupl_length(
     qubit: Mode,
     resonator: Mode,
     design_var_res_qb_coupl_length: Callable = n.design_var_coupl_length,
+    design_var_qubit_width: Callable = n.design_var_width,
 ) -> OptTarget:
 
     return OptTarget(
@@ -87,6 +95,7 @@ def get_opt_target_res_qub_chi_via_coupl_length(
         design_var_constraint={"larger_than": "5um", "smaller_than": "1000um"},
         prop_to=lambda p, v: np.abs(
             v[design_var_res_qb_coupl_length(resonator, qubit)]
+            / v[design_var_qubit_width(qubit)]
             * p[param_nonlin(qubit, qubit)]
             / (
                 p[param(qubit, c.FREQ)]
@@ -107,11 +116,32 @@ def get_opt_targets_qb_res_transmission(
     opt_target_resonator_freq=True,
     opt_target_resonator_kappa=True,
     opt_target_resonator_qubit_chi=True,
-    design_var_qubit_lj: Callable = n.design_var_lj,
-    design_var_qubit_width: Callable = n.design_var_width,
-    design_var_res_length: Callable = n.design_var_length,
-    design_var_res_coupl_length: Callable = n.design_var_coupl_length,
+    design_var_qubit_lj: Callable[[str], str] = n.design_var_lj,
+    design_var_qubit_width: Callable[[str], str] = n.design_var_width,
+    design_var_res_length: Callable[[str], str] = n.design_var_length,
+    design_var_res_coupl_length: Callable[[str, str], str] = n.design_var_coupl_length,
 ) -> List[OptTarget]:
+    """Get the optimization targets for a qubit-resonator system.
+    If you want to not include any of the design_var in the prop_to expressions
+    you can overwrite the defaults with the function discard_design_var_single or discard_design_var_double.
+
+    Args:
+        qubit (Mode): The qubit mode.
+        resonator (Mode): The resonator mode.
+        resonator_coupled_identifier (str): The identifier of the resonator coupled to the qubit.
+        opt_target_qubit_freq (bool, optional): Whether to optimize the qubit frequency. Defaults to True.
+        opt_target_qubit_anharm (bool, optional): Whether to optimize the qubit anharmonicity. Defaults to True.
+        opt_target_resonator_freq (bool, optional): Whether to optimize the resonator frequency. Defaults to True.
+        opt_target_resonator_kappa (bool, optional): Whether to optimize the resonator linewidth. Defaults to True.
+        opt_target_resonator_qubit_chi (bool, optional): Whether to optimize the qubit-resonator coupling strength. Defaults to True.
+        design_var_qubit_lj (Callable, optional): The function to get the qubit inductance. Defaults to n.design_var_lj.
+        design_var_qubit_width (Callable, optional): The function to get the qubit width. Defaults to n.design_var_width.
+        design_var_res_length (Callable, optional): The function to get the resonator length. Defaults to n.design_var_length.
+        design_var_res_coupl_length (Callable, optional): The function to get the resonator coupling length. Defaults to n.design_var_coupl_length.
+
+    Returns:
+        List[OptTarget]: The optimization targets.
+    """
     opt_targets = []
 
     if opt_target_qubit_freq:
@@ -149,6 +179,7 @@ def get_opt_targets_qb_res_transmission(
                 qubit,
                 resonator,
                 design_var_res_qb_coupl_length=design_var_res_coupl_length,
+                design_var_qubit_width=design_var_qubit_width,
             )
         )
     return opt_targets
