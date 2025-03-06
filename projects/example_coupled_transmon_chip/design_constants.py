20f168f299aeb4f5508cd49afe50fea09ecac154
LJSplitthoff <lukas.splitthoff@gmail.com>
2025-03-06 16:37:10 +0100

    Update design_constants.py

--- projects/example_coupled_transmon_chip/design_constants.py
+++ projects/example_coupled_transmon_chip/design_constants.py
@@ -2,7 +2,8 @@
 LINE_50_OHM_WIDTH = "16.51um"
 LINE_50_OHM_GAP = "10um"
 
-RESONATOR_WIDTH = "20um"
-RESONATOR_GAP = "20um"
+# these values yielding a 50Ohm transmission line are solely used in the check_kappa_estimate_basedon_eigenmodestudy branch
+RESONATOR_WIDTH = "16.51um"
+RESONATOR_GAP = "10um"
 
 BEND_RADIUS = "99um"
