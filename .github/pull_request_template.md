b332d15eb53f86f4bebe95c5c811c34e6d7854cb
PietroCampana <PietroCampana@users.noreply.github.com>
2025-03-14 12:27:13 +0000

    deploy: a1e9dab43ad85d2ccbe2a10b505d6600a6859390

--- .github/pull_request_template.md
+++
@@ -1,6 +0,0 @@
-Brief description of changes:
-
-Checklist before merge:
-- [ ] Reviewers confirm new code works as expected.
-- [ ] Tests are passing.
-- [ ] Documentation is updated.
--- src/qdesignoptimizer/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Quantum design optimizer package for automating the design of superconducting circuits."""
-
-import importlib.metadata as im
-
-__version__ = im.version(__package__)
